3375f91ed16c90be1e122a0d16ebad300aaabb06
KotelnikovSerV <serg.kotelnikov2003@yandex.ru>
2023-07-10 02:44:07 +0300

    гавно

--- routes.js
+++ routes.js
@@ -4,13 +4,8 @@ const router = (app) => {
         pool.query('SELECT * FROM Napravlenia', (error, result) => {
           if (error) res.send(error);
           else{
-            var unPure = {};
-            for (let i = 0; i < result.length; i++) {
-                unPure["row"+i] = ("row"+i ,result[i]);
-                
-            }
             //res.send(result)
-            res.render("first page",unPure);
+            res.render("first page",{data:result});
           };
         });
       });
