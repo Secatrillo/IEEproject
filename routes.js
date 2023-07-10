20744bc251a5d07f2277e25354402d18fd1c4d34
Secatrillo <106627730+Secatrillo@users.noreply.github.com>
2023-07-10 19:15:18 +0300

    da

--- routes.js
+++ routes.js
@@ -22,8 +22,8 @@ const router = (app) => {
           if (error) res.send(error);
           else{
             
-            res.send(result)
-            //res.render("fird page",{data: result});
+            //res.send(result)
+            res.render("fird page",{data: result[0]});
           };
         });
       });
@@ -34,8 +34,8 @@ const router = (app) => {
           if (error) res.send(error);
           else{
             
-            res.send(result)
-            //res.render("fird page",{data: result});
+            //res.send(result)
+            res.render("fird page",{data: result[0]});
           };
         });
       });
@@ -46,8 +46,8 @@ const router = (app) => {
           if (error) res.send(error);
           else{
             
-            res.send(result)
-            //res.render("fird page",{data: result});
+            //res.send(result)
+            res.render("fird page",{data: result[0]});
           };
         });
       });
@@ -58,26 +58,13 @@ const router = (app) => {
           if (error) res.send(error);
           else{
             
-            res.send(result)
-            //res.render("fird page",{data: result});
+            //res.send(result)
+            res.render("fird page",{data: result[0]});
           };
         });
       });
       
-      app.get('/about', (req, res) => {
-        pool.query('SELECT * FROM Napravlenia', (error, result) => {
-          if (error) res.send(error);
-          else{
-            var unPure = {};
-            for (let i = 0; i < result.length; i++) {
-                unPure["row"+i] = ("row"+i ,result[i]);
-                
-            }
-            //res.send(unPure)
-            res.render("second page",unPure);
-          };
-        });
-      });
+
       
 };
 export default router;
