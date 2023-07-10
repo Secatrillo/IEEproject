e5a767c2fdafb7c250bfb291da14ac95823e8f5e
Georushka <138894157+Georushka@users.noreply.github.com>
2023-07-10 21:29:34 +0300

    1

--- routes.js
+++ routes.js
@@ -14,8 +14,52 @@ const router = (app) => {
           };
         });
       });
+      
+      app.get('/erza', (req, res) => {
+        pool.query('SELECT * FROM ERZA', (error, result) => {
+          if (error) res.send(error);
+          else{
+    
+            res.render("second page",{data: result, link:"erza"});
+          };
+        });
+      });
 
+      app.get('/raspseti', (req, res) => {
+        pool.query('SELECT * FROM RASPSETI', (error, result) => {
 
+          if (error) res.send(error);
+          else{
+            
+            //res.send(result)
+            res.render("second page",{data: result, link:"raspseti"});
+          };
+        });
+      });
+
+      app.get('/electroenergy', (req, res) => {
+        pool.query('SELECT * FROM ELECTROENERGY', (error, result) => {
+
+          if (error) res.send(error);
+          else{
+            
+            //res.send(result)
+            res.render("second page",{data: result, link:"electroenergy"});
+          };
+        });
+      });
+
+      app.get('/electroenergyetalon', (req, res) => {
+        pool.query('SELECT * FROM ELECTROENERGYETALON', (error, result) => {
+
+          if (error) res.send(error);
+          else{
+            
+            //res.send(result)
+            res.render("second page",{data: result, link:"electroenergyetalon"});
+          };
+        });
+      });
 
       app.get("/erza/:link", (req, res) => {
         const link = req.params.link;
@@ -23,28 +67,6 @@ const router = (app) => {
           if (error) res.send(error);
           else{
     
-            res.render("fird page",{data: result[0]});
-          };
-        });
-      });
-      
-      app.get('/erza', (req, res) => {
-        pool.query('SELECT * FROM ERZA', (error, result) => {
-          if (error) res.send(error);
-          else{
-    
-            res.render("second page",{data: result});
-          };
-        });
-      });
-
-      app.get('/raspseti', (req, res) => {
-        pool.query('SELECT * FROM Napravlenia', (error, result) => {
-
-          if (error) res.send(error);
-          else{
-            
-            //res.send(result)
             res.render("fird page",{data: result[0]});
           };
         });
