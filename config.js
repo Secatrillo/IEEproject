3404223808707dc09d940bf85e9b1bc7d3f67792
Secatrillo <106627730+Secatrillo@users.noreply.github.com>
2023-07-15 20:56:25 +0300

    newbaze

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql2';
 const config = {
     host: 'sql7.freesqldatabase.com',
-    user: 'sql7631273',
-    password: 'nKpX4ElVeU',
-    database: 'sql7631273',
+    user: 'sql7632981',
+    password: 'H16SstkuFF',
+    database: 'sql7632981',
 };
 // Create a MySQL pool 
 const pool = mysql.createPool(config);
